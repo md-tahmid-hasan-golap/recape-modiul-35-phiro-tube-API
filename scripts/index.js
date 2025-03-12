033e98aa38dbf91a5131d83f36b2885d348cb46c
golap-raj <golapraj47@gmail.com>
2025-03-13 01:47:08 +0600

    add nav bar

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+
+// load function
+function load (){
+    // fetch chalabo
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then(res => res.json())
+    .then(data => display(data.categories))
+}
+
+load()
+
+// display function
+
+function display (categories){
+    // parent div 
+
+    const categoriesParent = document.getElementById("categories");
+    
+
+    // loop chalabo 
+    for(let categori of categories ){
+        // console.log(categori)
+            
+        // creat Element
+
+        const btn_div = document.createElement("categories");
+        btn_div.innerHTML = `
+             <button class="btn hover:bg-[#FF1F3D] hover:text-white">${categori.category}</button>
+        `
+        //  appendChild div
+        
+        categoriesParent.appendChild(btn_div)
+
+        
+    }
+
+}
